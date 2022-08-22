49daf09a10aad09090a0221a545ae0b3c831fb88
sistemas soporte <sistemas@aassa.local>
2022-08-22 17:08:27 -0500

    Se crea el archivo cupones.js, index y se agrega imagen, se inicia proceso de crear formulario

---
+++ cupones.js
@@ -0,0 +1,9 @@
+const coupons = [
+  'JuanDC_es_Batman',
+  'pero_no_le_digas_a_nadie',
+  'es_un_secreto',
+];
+
+function onclickValidarCupon() {
+  const inputCupon = document.getElementById('InputCupon').value;
+}
