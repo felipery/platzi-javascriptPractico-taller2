e94ee5ed0bd8a66457d72696ed3ac4a0b68ff13f
sistemas soporte <sistemas@aassa.local>
2022-08-24 14:35:45 -0500

    Se reestructura html poniendo un input para el precio, adicionalmente se crea un array con objetos en su interior para mejorar el código

--- cupones.js
+++ cupones.js
@@ -1,9 +1,25 @@
 const coupons = [
-  'JuanDC_es_Batman',
-  'pero_no_le_digas_a_nadie',
-  'es_un_secreto',
+  {
+    name: 'JuanDC_es_Batman',
+    discount: 15,
+  },
+  {
+    name: 'pero_no_le_digas_a_nadie',
+    discount: 30,
+  },
+  {
+    name: 'es_un_secreto',
+    discount: 25,
+  },
 ];
 
+function calcularDescuento(descuento) {
+  return;
+}
+
 function onclickValidarCupon() {
+  const inputPrice = document.getElementById('InputPrice').value;
   const inputCupon = document.getElementById('InputCupon').value;
+  console.log(inputPrice);
+  console.log(inputCupon);
 }
